d871fd60221087227f94ddf55d41d523eb9f9d4f
karkiayush <karkiayush619@gmail.com>
2024-01-15 13:37:59 +0545

    code is optimized

--- LabFirst/LabFirstBisectionMethod.c
+++ LabFirst/LabFirstBisectionMethod.c
@@ -2,17 +2,11 @@
 #include <stdbool.h>
 #include <math.h>
 
+int iter = 0;
 bool isFunctionalValuePositive(double X)
 {
     double Fx = pow(X, 3) - (2 * X) - 5;
-    if (Fx > 0)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
+    return (Fx > 0);
 }
 
 double bisectionMethodCalculation(double Xu, double Xl, double tolerance)
@@ -20,7 +14,7 @@ double bisectionMethodCalculation(double Xu, double Xl, double tolerance)
     double X;
     while (fabs(Xu - Xl) >= tolerance)
     {
-
+        iter++;
         X = (Xu + Xl) / 2;
         bool functionResult = isFunctionalValuePositive(X);
 
@@ -48,7 +42,7 @@ int main()
 
     double root = bisectionMethodCalculation(Xu, Xl, tolerance);
 
-    printf("The root of the equation X^3 - 2X - 5 is: %lf", root);
+    printf("The root of the equation X^3 - 2X - 5 is: %lf which is founded in total of %d iteration", root, iter);
 
     return 0;
 }
